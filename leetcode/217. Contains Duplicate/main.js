962d3287cb9a160f75ed924d8c915cbf371afb2f
Rifat Khan <rifat6633@gmail.com>
2025-04-01 17:27:02 +0600

    assign true instade of 1.

--- leetcode/217. Contains Duplicate/main.js
+++ leetcode/217. Contains Duplicate/main.js
@@ -7,7 +7,7 @@ var containsDuplicate = function (nums) {
   const hashTable = {};
   for (let i = 0; i < nums.length; i++) {
     if (!hashTable[nums[i]]) {
-      hashTable[nums[i]] = 1;
+      hashTable[nums[i]] = true;
     } else {
       return true;
     }
