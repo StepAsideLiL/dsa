14eb6aa8d6c5e527ceb664b6a721449e4a541557
Rifat Khan <rifat6633@gmail.com>
2025-04-04 10:23:43 +0600

    change to 1.

--- leetcode/217. Contains Duplicate/main.js
+++ leetcode/217. Contains Duplicate/main.js
@@ -7,7 +7,7 @@ var containsDuplicate = function (nums) {
   const hashTable = {};
   for (let i = 0; i < nums.length; i++) {
     if (!hashTable[nums[i]]) {
-      hashTable[nums[i]] = true;
+      hashTable[nums[i]] = 1;
     } else {
       return true;
     }
