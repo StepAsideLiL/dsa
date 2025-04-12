ddec212a7044cfa5bc20e907187d609b67175761
Rifat Khan <rifat6633@gmail.com>
2025-04-12 10:41:22 +0600

    add solution 2 of 347. Top K Frequent Elements.

--- leetcode/347. Top K Frequent Elements - m/js/main.js
+++ leetcode/347. Top K Frequent Elements - m/js/main.js
@@ -1,17 +1,26 @@
+const { MinPriorityQueue } = require("@datastructures-js/priority-queue");
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-  // Solution 2:
+  // Solution 2 (min heap): Time Complexity: O(n log k), Space Complexity: O(n)
   const count = {};
   for (const num of nums) {
     count[num] = (count[num] || 0) + 1;
   }
 
   const heap = new MinPriorityQueue((x) => x[1]);
-  return heap;
+  for (const [num, f] of Object.entries(count)) {
+    heap.enqueue([num, f]);
+    if (heap.size() > k) {
+      heap.dequeue();
+    }
+  }
+
+  return heap.toArray().map((res) => parseInt(res[0]));
 
   // Solution 1 (hash sorting): Time Complexity: O(n log n), Space Complexity: O(n)
   // const count = {};
@@ -26,5 +35,7 @@ var topKFrequent = function (nums, k) {
 };
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+console.log("===");
 console.log(topKFrequent([1, 1, 1, 6, 6, 6, 6, 2, 2, 3], 2));
+console.log("===");
 console.log(topKFrequent([1], 1));
