1b576af475977f2acc3eb21115433dc2145a2b92
Rifat Khan <rifat6633@gmail.com>
2025-04-12 08:26:11 +0600

    put js code in js dir.

---
+++ leetcode/347. Top K Frequent Elements - m/js/main.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  // Solution 2:
+  const count = {};
+  for (const num of nums) {
+    count[num] = (count[num] || 0) + 1;
+  }
+
+  const heap = new MinPriorityQueue((x) => x[1]);
+  return heap;
+
+  // Solution 1 (hash sorting): Time Complexity: O(n log n), Space Complexity: O(n)
+  // const count = {};
+  // for (let i = 0; i < nums.length; i++) {
+  //   count[nums[i]] = (count[nums[i]] || 0) + 1;
+  // }
+  // return Object.entries(count)
+  //   .map(([num, f]) => [f, parseInt(num)])
+  //   .sort((a, b) => b[0] - a[0])
+  //   .slice(0, k)
+  //   .map((p) => p[1]);
+};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+console.log(topKFrequent([1, 1, 1, 6, 6, 6, 6, 2, 2, 3], 2));
+console.log(topKFrequent([1], 1));
