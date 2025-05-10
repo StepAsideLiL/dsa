47683f54a1741e774fa22bc83d70b5ac6ec9b127
Rifat Khan <rifat6633@gmail.com>
2025-05-10 10:01:08 +0600

    add solution 1 for 167. Two Sum II - Input Array Is Sorted problem.

---
+++ leetcode/167. Two Sum II - Input Array Is Sorted - m/js/sol1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  // Solution 1 (Brute Force): Time Complexity: O(n^2), Space Complexity: O(n)
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 3, 4], 6));
+console.log(twoSum([-1, 0], -1));
