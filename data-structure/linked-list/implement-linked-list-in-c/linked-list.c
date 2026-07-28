38d91e3a7e8b5f3d7d48ae62d4abe48fcc929000
Rifat Khan <rifat6633@gmail.com>
2026-07-28 08:23:59 +0600

    refactor and update.

--- data-structure/linked-list/implement-linked-list-in-c/linked-list.c
+++ data-structure/linked-list/implement-linked-list-in-c/linked-list.c
@@ -37,9 +37,16 @@ typedef struct LinkedList
   /**
    * @brief Head node.
    *
-   * This node stores the head node of the linked list.
+   * This node is the head node of the linked list.
    */
   struct Node *head;
+
+  /**
+   * @brief Last Node.
+   *
+   * This node is the last node of the linked list.
+   */
+  struct Node *last;
 
   /**
    * @brief Size
@@ -69,7 +76,7 @@ typedef struct LinkedList
  * @param type Type of linked list.
  * @return `LinkedList` if successful, `NULL` otherwise
  */
-LinkedList *createList(int type)
+LinkedList *create_list(int type)
 {
   if (type >= 1 && type <= 4)
   {
@@ -98,7 +105,7 @@ LinkedList *createList(int type)
  * @param value Node data.
  * @return node if successful.
  */
-Node *createNode(int value)
+Node *create_node(int value)
 {
   Node *node = (Node *)malloc(sizeof(Node));
 
@@ -123,13 +130,36 @@ Node *createNode(int value)
  * @param headRef Pointer reference of the linked list head.
  * @param value Node data.
  */
-void insertInEmpty(Node **headRef, int value)
+void insert_in_empty(LinkedList *list, int value)
 {
-  Node *newNode = createNode(value);
+  Node *node = create_node(value);
 
-  newNode->next = newNode;
-  newNode->prev = newNode;
-  *headRef = newNode;
+  switch (list->type)
+  {
+  case 1:
+    node->next = NULL;
+    node->prev = NULL;
+    break;
+  case 2:
+    node->next = NULL;
+    node->prev = NULL;
+    break;
+  case 3:
+    node->next = node;
+    node->prev = NULL;
+    break;
+  case 4:
+    node->next = node;
+    node->prev = node;
+    break;
+  default:
+    printf("\nType %d does not exists.\n\n", list->type);
+    break;
+  }
+
+  list->head = node;
+  list->last = node;
+  list->size++;
 }
 
 /**
@@ -140,25 +170,50 @@ void insertInEmpty(Node **headRef, int value)
  * @param headRef Pointer reference of the linked list head.
  * @param value Node data.
  */
-void insertAtHead(Node **headRef, int value)
+void insert_at_head(LinkedList *list, int value)
 {
-  if (*headRef == NULL)
+  if (list->head == NULL)
   {
-    insertInEmpty(headRef, value);
+    insert_in_empty(list, value);
     return;
   }
 
-  Node *head = *headRef;
-  Node *last = head->prev;
-  Node *newNode = createNode(value);
+  Node *head = list->head;
+  Node *last = list->last;
+  Node *newNode = create_node(value);
 
-  newNode->next = head;
-  newNode->prev = last;
-  last->next = newNode;
-  head->prev = newNode;
+  switch (list->type)
+  {
+  case 1:
+    newNode->next = head;
+    break;
+  case 2:
+    newNode->next = head;
+    head->prev = newNode;
+    break;
+  case 3:
+    newNode->next = head;
+    last->next = newNode;
+    break;
+  case 4:
+    newNode->next = head;
+    newNode->prev = last;
+    last->next = newNode;
+    head->prev = newNode;
+    break;
+  default:
+    printf("\nType %d does not exists.\n\n", list->type);
+    break;
+  }
+
+  // newNode->next = head;
+  // newNode->prev = last;
+  // last->next = newNode;
+  // head->prev = newNode;
 
   // Update head pointer
-  *headRef = newNode;
+  list->head = newNode;
+  list->size++;
 }
 
 /**
@@ -169,23 +224,50 @@ void insertAtHead(Node **headRef, int value)
  * @param headRef Pointer reference of the linked list head.
  * @param value Node data.
  */
-void insertAtTail(Node **headRef, int value)
+void insert_at_tail(LinkedList *list, int value)
 {
-  if (*headRef == NULL)
+  if (list->head == NULL)
   {
-    insertInEmpty(headRef, value);
+    insert_in_empty(list, value);
     return;
   }
 
-  Node *head = *headRef;
-  Node *last = head->prev;
-  Node *newNode = createNode(value);
+  Node *head = list->head;
+  Node *last = list->last;
+  Node *newNode = create_node(value);
 
-  newNode->next = head;
-  newNode->prev = last;
-  last->next = newNode;
-  head->prev = newNode;
+  switch (list->type)
+  {
+  case 1:
+    last->next = newNode;
+    break;
+  case 2:
+    last->next = newNode;
+    newNode->prev = last;
+    break;
+  case 3:
+    last->next = newNode;
+    newNode->next = head;
+    break;
+  case 4:
+    newNode->next = head;
+    newNode->prev = last;
+    last->next = newNode;
+    head->prev = newNode;
+    break;
+  default:
+    printf("\nType %d does not exists.\n\n", list->type);
+    break;
+  }
+
+  // newNode->next = head;
+  // newNode->prev = last;
+  // last->next = newNode;
+  // head->prev = newNode;
+
   // head stays the same, newNode becomes the new 'last'
+  list->last = newNode;
+  list->size++;
 }
 
 // Delete the first node matching 'key'
@@ -197,15 +279,15 @@ void insertAtTail(Node **headRef, int value)
  * @param headRef Pointer reference of the linked list head.
  * @param key Key to delete.
  */
-void deleteValue(Node **headRef, int key)
+void deleteValue(LinkedList *list, int key)
 {
-  if (*headRef == NULL)
+  if (list->head == NULL)
   {
     printf("List is empty.\n");
     return;
   }
 
-  Node *curr = *headRef;
+  Node *curr = list->head;
 
   // Search for the node containing key
   do
@@ -213,7 +295,7 @@ void deleteValue(Node **headRef, int key)
     if (curr->data == key)
       break;
     curr = curr->next;
-  } while (curr != *headRef);
+  } while (curr != list->head);
 
   // Key was not found
   if (curr->data != key)
@@ -226,14 +308,14 @@ void deleteValue(Node **headRef, int key)
   if (curr->next == curr)
   {
     free(curr);
-    *headRef = NULL;
+    list->head = NULL;
     return;
   }
 
   // Case 2: Deleting the head node
-  if (curr == *headRef)
+  if (curr == list->head)
   {
-    *headRef = curr->next;
+    list->head = curr->next;
   }
 
   // Unlink the node from both neighbors
@@ -321,28 +403,43 @@ void freeList(Node **headRef)
 /**
  * @brief Clear the terminal.
  */
-void clearScreen()
+void clear_screen()
 {
   // \e[1J clears the screen, \e[H moves cursor to the top-left corner
   printf("\e[1J\e[H");
 }
 
+/**
+ * @brief Interrupts the program.
+ *
+ * This function interrupts the program to print some warning or error before clearing the screen.
+ */
+void press_enter_to_continue()
+{
+  // clear input buffer.
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+
+  printf("Press ENTER to continue...");
+  getchar();
+}
+
 int main()
 {
-  bool running = true;
+  bool running = false;
   char choice;
   int operation;
-  int value;
+  int inputValue;
 
   LinkedList *list = NULL;
-
   int linkedListType;
 
   while (running)
   {
     if (!list)
     {
-      clearScreen();
+      clear_screen();
       printf("=== C Linked List ===\n");
 
       printf("Create A Linked List (y/n): ");
@@ -350,10 +447,20 @@ int main()
 
       if (choice == 'y' || choice == 'Y')
       {
-        printf("Add the first node (head) value: ");
-        scanf(" %d", &value);
+        printf("Types of Linked List.\n");
+        printf("1 - Singly Linked List.\n");
+        printf("2 - Doubly Linked List.\n");
+        printf("3 - Circular Singly Linked List.\n");
+        printf("4 - Circular Doubly Linked List.\n");
+        printf("Enter the type of linked list (1 - 4): ");
+        scanf(" %d", &inputValue);
 
-        insertInEmpty(&list->head, value);
+        list = create_list(inputValue);
+
+        if (!list)
+        {
+          press_enter_to_continue();
+        }
       }
       else if (choice == 'n' || choice == 'N')
       {
@@ -363,17 +470,16 @@ int main()
       }
       else
       {
-        while (getchar() != '\n')
-          ;
-        clearScreen();
+        clear_screen();
         printf("Invalid input.\n\n");
+        press_enter_to_continue();
         continue;
       }
     }
     else
     {
       printf("\n");
-      printf("Menus");
+      printf("Menus\n");
       printf("1. Insert new value at the head\n");
       printf("2. Insert new value at the tail\n");
       printf("3. Delete a node by key data\n");
@@ -392,6 +498,16 @@ int main()
 
       switch (operation)
       {
+      case 1:
+        printf("Value: ");
+        scanf("%d", &inputValue);
+        insert_at_head(list, inputValue);
+        break;
+      case 2:
+        printf("Value: ");
+        scanf("%d", &inputValue);
+        insert_at_tail(list, inputValue);
+        break;
       case 6:
         freeList(&list->head);
         running = false;
