7f5bddd4c615c6dfd68512a70a68bd2e55212502
Rifat Khan <rifat6633@gmail.com>
2026-07-30 23:44:33 +0600

    update array.

--- data-structure/array/dynamic-array-in-c/array.c
+++ data-structure/array/dynamic-array-in-c/array.c
@@ -177,14 +177,107 @@ void printArray(DynamicArray *d)
   printf("[");
   for (int i = 0; i < d->length; i++)
   {
-    printf("%d%s", d->data[i], i == d->length - 1 ? ", " : "");
+    printf("%d%s", d->data[i], i != d->length - 1 ? ", " : "");
   }
   printf("]\n");
   printf("Array lenght: %d\n", d->length);
   printf("Array capasity: %d\n", d->capacity);
 }
 
+/**
+ * @brief Clear the terminal.
+ */
+void clearScreen()
+{
+  // \e[1J clears the screen, \e[H moves cursor to the top-left corner
+  printf("\e[1J\e[H");
+}
+
+/**
+ * @brief Interrupts the program.
+ *
+ * This function interrupts the program to print some warning or error before clearing the screen.
+ */
+void pressEnterToContinue()
+{
+  // clear input buffer.
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
+
+  printf("Press ENTER to continue...");
+  getchar();
+}
+
 int main()
 {
+  bool running = true;
+  char choice;
+  int operation;
+  int inputValue;
+
+  DynamicArray *d = NULL;
+
+  while (running)
+  {
+    if (!d)
+    {
+      clearScreen();
+      printf("=== C Dynamic Array ===\n");
+
+      printf("Create a dynamic array (y/n): ");
+      scanf(" %c", &choice);
+      if (choice == 'y' || choice == 'Y')
+      {
+        d = createDynamicArray(0);
+      }
+      else if (choice == 'n' || choice == 'N')
+      {
+        running = false;
+        printf("Exiting application, bye!\n");
+      }
+      else
+      {
+        printf("Invalid input!\n");
+        pressEnterToContinue();
+      }
+    }
+    else
+    {
+      printf("Array lenght: %d\n", d->length);
+      printArray(d);
+      printf("Menus\n");
+      printf("1. Push a value.\n");
+      printf("2. Pop a value.\n");
+      printf("3. Free array.\n");
+      printf("4. Exit application\n");
+      printf("Enter your choice (1 - 4): ");
+      scanf("%d", &operation);
+
+      switch (operation)
+      {
+      case 1:
+        printf("Value: ");
+        scanf("%d", &inputValue);
+        push(d, inputValue);
+        break;
+      case 2:
+        pop(d);
+        break;
+      case 3:
+        free(d->data);
+        free(d);
+        break;
+      case 4:
+        running = false;
+        printf("Exiting application, bye!\n");
+        break;
+      default:
+        printf("%d is not a valid operation.\n", operation);
+        break;
+      }
+    }
+  }
+
   return 0;
 }
